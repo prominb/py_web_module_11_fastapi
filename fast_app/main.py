00891bbbf782b6b0bf0585f3255f6709223bda93
Roman M <prominb@gmail.com>
2024-05-14 07:17:55 +0300

    e002 path and query

--- fast_app/main.py
+++ fast_app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
@@ -6,3 +6,24 @@ app = FastAPI()
 @app.get("/api/healthchecker")
 def root():
     return {"message": "Welcome to FastAPI!"}
+
+@app.get("/note/new")
+async def read_new_notes():
+    return {"message": "Return new notes"}
+
+# @app.get("/notes/{note_id}")
+# async def read_note(note_id: int):
+#     return {"note": note_id}
+@app.get("/notes/{note_id}")
+async def read_note(note_id: int = Path(description="The ID of the note to get", gt=0, le=10)):
+    return {"note": note_id}
+
+# @app.get("/notes")
+# async def read_notes(skip: int = 0, limit: int = 10):
+#     return {"message": f"Return all notes: skip: {skip}, limit: {limit}"}
+# @app.get("/notes")
+# async def read_notes(skip: int = 0, limit: int = 10, q: str | None = None):
+#     return {"message": f"Return all notes: skip: {skip}, limit: {limit}"}
+@app.get("/notes")
+async def read_notes(skip: int = 0, limit: int = Query(default=10, le=100, ge=10)):
+    return {"message": f"Return all notes: skip: {skip}, limit: {limit}"}
