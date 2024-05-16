dbd9cac11aee96fb0648061ff1eb768fdc191aa0
Roman_M <prominb@gmail.com>
2024-05-16 17:47:19 +0300

    e106 ADD app3, app4

---
+++ fast_app/app3.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Path, Query, Depends, HTTPException
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from db import get_db
+
+app = FastAPI()
+
+
+@app.get("/api/healthchecker")
+def healthchecker(db: Session = Depends(get_db)):
+    try:
+        # Make request
+        result = db.execute("SELECT 1").fetchone()
+        if result is None:
+            raise HTTPException(status_code=500, detail="Database is not configured correctly")
+        return {"message": "Welcome to FastAPI!"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Error connecting to the database")
+...
---
+++ fast_app/app4.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
+
+from db import get_db, Note
+
+app = FastAPI()
+
+...
+
+class NoteModel(BaseModel):
+    name: str
+    description: str
+    done: bool
+
+
+@app.post("/notes")
+async def create_note(note: NoteModel, db: Session = Depends(get_db)):
+    new_note = Note(name=note.name, description=note.description, done=note.done)
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
+    return new_note
